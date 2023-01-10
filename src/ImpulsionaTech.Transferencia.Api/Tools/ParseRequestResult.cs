336b21b041258fb5e95947e8b02ce578b8e90525
Mateus Macedo <mateusouza2014@live.com>
2023-01-10 19:49:24 -0300

    feat: Adicionando estrutura inicial do projeto

---
+++ src/ImpulsionaTech.Transferencia.Api/Tools/ParseRequestResult.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImpulsionaTech.Transferencia.Api.Tools
+{
+    public class ParseRequestResult
+    {
+
+    }
+}
